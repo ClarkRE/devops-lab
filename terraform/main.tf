ed5face513d9d9a9369b61b34036002d3d21daac
ClarkRE <rearlclark@hotmail.com>
2026-07-22 07:48:42 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
 
+provider "docker" {}
+
+resource "docker_container" "nginx" {
+  image = "nginx:latest"
+  name  = "nginx_server"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
